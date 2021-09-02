b03ca4fa1cd553d1e28a214412c4febaeda26234
unknown <creativo@agenciamanager.com>
2021-09-02 08:12:04 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), //52454544122
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Labore commodo esse irure culpa in aute nostrud quis do ullamco officia tempor ea cupidatat.',
+            picture: null,            
+        },
+        {
+            id: new Date().getTime() + 10000, //52454544122
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Consectetur deserunt ex nostrud ut nulla mollit ad exercitation velit ea aliqua id ullamco ex. In pariatur tempor cillum cupidatat. Ipsum duis esse dolore pariatur pariatur minim amet labore aliqua aliquip qui. In anim irure fugiat exercitation mollit anim aute. Incididunt pariatur Lorem ut ullamco sint. Qui voluptate cillum deserunt laboris dolor amet nisi Lorem tempor incididunt sit minim. Dolor culpa duis commodo ipsum voluptate officia voluptate. Eu fugiat velit esse laborum ullamco cupidatat deserunt sint magna consequat laborum. Adipisicing irure amet est labore adipisicing proident esse ut minim adipisicing amet. Commodo qui enim consectetur cillum et eu commodo. Occaecat est Lorem nisi aliqua. Elit laborum laboris culpa magna ad nostrud commodo nostrud nostrud. Ipsum consequat officia proident mollit consequat aliquip aliqua nisi.',
+            picture: null,            
+        },
+        {
+            id: new Date().getTime() + 151505 , //52454544122
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Labore commodo esse irure culpa in aute nostrud quis do ullamco officia tempor ea cupidatat.',
+            picture: null,            
+        },
+    ]
+})
+
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,64 @@
+import { defineAsyncComponent } from 'vue';
+<template>
+    <div class="entry-title d-flex justify-content-between p-2">
+        <div>
+            <span class="text-sucess fs-3 fw-bold">15</span>
+            <span class="mx-1 fs-3">Julio</span>
+            <span class="mx-2 fs-4 fw-light">2021, jueves</span>
+        </div>
+
+        <div>
+            <button class="btn btn-danger mx-2">
+                borrar
+                <i class="fa fa-trash-alt"></i>
+            </button>
+            <button class="btn btn-primary">
+                subir foto
+                <i class="fa fa-upload"></i>
+            </button>
+        </div>
+    </div>
+    <hr>
+    <div class="d-flex flex-column px-3 h-75">
+        <textarea placeholder="¿Qué sucedió hoy?"></textarea>
+    </div>
+    
+    <Fab icon="fa-save" />
+
+    <img 
+        src="https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" alt="entry-picture"
+        class="img-thumbnail">
+</template>
+
+<script>
+
+import {defineAsyncComponent} from 'vue'
+
+export default {
+    components:{
+        Fab: defineAsyncComponent(() => import(/*webpackChunkName: "Fab"*/'../components/Fab.vue'))
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+textarea{
+    font-size: 20px;
+    border: none;
+    height: 100%;
+
+    &:focus{
+        outline: none;
+    }
+}
+
+img{
+    width: 200px;
+    position: fixed;
+    bottom: 150px;
+    right: 20px;
+    box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+}
+
+</style>
